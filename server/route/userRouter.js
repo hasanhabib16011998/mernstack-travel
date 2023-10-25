e79ba6ac464eb933256059b2cfaecc5b7dddcce3
hasanhabib16011998 <hasan16habib@gmail.com>
2023-10-25 11:57:04 +0600

    from linux

--- server/route/userRouter.js
+++ server/route/userRouter.js
@@ -1,11 +1,10 @@
-import {Router} from 'express';
-import{register,login} from '../controllers/user.js';
-import {auth} from '../middlewares/auth';
+import { Router } from 'express';
+import { register, login } from '../controllers/user.js';
+import auth from '../middlewares/auth.js'; // Use default import
 
-const userRouter=Router();
-userRouter.post('/register',register);
-userRouter.post('/login',login);
-userRouter.patch('/updateProfile',auth,updateProfile);
-
+const userRouter = Router();
+userRouter.post('/register', register);
+userRouter.post('/login', login);
+//userRouter.patch('/updateProfile', auth, updateProfile);
 
 export default userRouter;
